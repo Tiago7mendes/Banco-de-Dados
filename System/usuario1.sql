52a79e1487b47fe72399f4ea4ed7a5f06ca900f2
Tiago7mendes <tiagosettimendes@gmail.com>
2025-09-17 11:29:15 -0300

    usario1 e usuario2 priviliges

--- System/usuario1.sql
+++ System/usuario1.sql
@@ -3,3 +3,10 @@ create table Funcionario_func(
     nome varchar(20),
     salario number(10,2));
     
+-- Atribuir ao usuario2 os privilégios de:
+
+--◦ inserir tuplas na tabela funcionario de usuario1
+GRANT INSERT ON usuario1.funcionario TO usuario2;
+
+--◦ atualizar salario da tabela funcionario de usuario1
+GRANT UPDATE ( salario ) ON usuario1.funcionario TO usuario2;
--- System/usuario2.sql
+++ System/usuario2.sql
@@ -2,3 +2,7 @@ create table Emp(
     emp_id int primary key,
     nome varchar(20),
     salario number(5,2));
+
+insert into usuario1.Funcionario(cod_func, nome) values(1, 'Ana');
+
+select * from usuario1.funcionario;
